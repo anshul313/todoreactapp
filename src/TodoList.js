dae6bbbea47dbf5142c2495647097e46139d909f
anshul313 <anshulsharma.sharma997@gmail.com>
2023-06-24 14:36:17 +0530

    todo app using reactjs

---
+++ src/TodoList.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+const TodoList = () => {
+  const [activity, setactivity] = useState("");
+  const [listData, setlistData] = useState([]);
+
+  function addactivity() {
+    // setlistData([...listData,activity]);
+    // console.log(listData);
+    setlistData((listData) => {
+      const updatedlist = [...listData, activity];
+      console.log("updated list is ", updatedlist);
+      setactivity("");
+      return updatedlist;
+    });
+  }
+
+  function removeActivity(i) {
+    const updatedListData = listData.filter((ele, id) => {
+      return i != id;
+    });
+    setlistData(updatedListData);
+  }
+
+  function removeall() {
+    setlistData([]);
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="header">TODO LIST</div>
+        <input
+          type="text"
+          placeholder="add activity"
+          value={activity}
+          onChange={(e) => setactivity(e.target.value)}
+        />
+        <button onClick={addactivity} className="addbutton">Add +</button>
+        <p className="list-heading">Here is your List :{")"}</p>
+        {listData!=[] && listData.map((data,i)=>{
+          return(
+            <>
+            <p key={i} className="listdata">
+              <div className="listdata">{data}</div>
+              <button onClick={()=>removeActivity(i)} className="removebutton" >Remove(-)</button>
+            </p>
+            </>
+          )
+        })}
+        <button onClick={()=>removeall()} className="removeallbutton" >RemoveAll(-)</button>
+      </div>
+    </>
+  );
+};
+
+export default TodoList;
